f64d32fa0fb2ea20bdb958151b3ddedcce8103b7
Hans Fredrik Maast <hfmaast@gmail.com>
2022-12-05 00:50:48 +0100

    Update Filters.js

--- helpers/Filters.js
+++ helpers/Filters.js
@@ -5,13 +5,15 @@ const today = new Date();
 export const getShowsAhead = (shows = [], scene, days = 7) => {
   return shows.filter((show) => {
     const showDate = new Date(show.date);
-    
-    const isInDayRange = differenceInDays(showDate, today) <= days || isToday(showDate);
    
     let condition = isInDayRange;  
+  
+    if (day === 0) {
+      condition = isToday(showDate);
+    }
     
     if (scene) {
-      condition = isInDayRange && show.scene === scene;
+      condition = condition && show.scene === scene;
     }
      
     if (scene && days === Infinity) {
