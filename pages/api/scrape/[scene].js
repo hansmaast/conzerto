f28df624f9d293e9de4631225cf6c41693fe5a4d
Hans Maast <hans.maast@boligmappa.no>
2022-12-11 22:16:35 +0100

    Fixed build issue

--- pages/api/scrape/[scene].js
+++ pages/api/scrape/[scene].js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import * as fetchers from "scraper/fetchers";
+import * as fetchers from "../../../lib/scraper/fetchers";
 
 export default async function handler(req, res) {
   console.log("Auth: ", req.headers.authorization);
