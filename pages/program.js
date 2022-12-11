e21b225651cfdab908f59835e6f41665aca2fe1b
Hans Maast <hans.maast@boligmappa.no>
2022-12-11 23:01:23 +0100

    Updated imports, again

--- pages/program.js
+++ pages/program.js
@@ -4,7 +4,7 @@ import { Shows } from "../lib/components/Shows";
 import { StickyNavigation } from "../lib/components/StickyNavigation";
 import { useDateInView } from "../lib/hooks/useDateInView";
 import { useShows } from "../lib/hooks/useShows";
-import { fetchAllShows } from "../lib/scraper/fetchers";
+import { fetchAllShows } from "../lib/scraper";
 import styles from "../styles/Home.module.css";
 const title = "OSLO";
 
